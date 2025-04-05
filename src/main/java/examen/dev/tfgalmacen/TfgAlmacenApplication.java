d03d6cc896a989258b141af862ca18f9bd43cb1b
Raúl Fernández <146001066+rraul10@users.noreply.github.com>
2025-04-05 12:09:55 +0200

    Subida del proyecto

---
+++ src/main/java/examen/dev/tfgalmacen/TfgAlmacenApplication.java
@@ -0,0 +1,13 @@
+package examen.dev.tfgalmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgAlmacenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfgAlmacenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/examen/dev/tfgalmacen/TfgAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package examen.dev.tfgalmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgAlmacenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
