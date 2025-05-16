32de76aec10679ee469007b0cc2a20f4c3073e8f
Raul Fernandez <raulfernandez6106@gmail.com>
2025-05-16 12:15:40 +0200

    Arreglado  Pedidos

--- src/main/java/examen/dev/tfgalmacen/rest/pedido/repository/PedidoRepository.java
+++ src/main/java/examen/dev/tfgalmacen/rest/pedido/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package examen.dev.tfgalmacen.rest.pedido.repository;
 
 import examen.dev.tfgalmacen.rest.pedido.models.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,9 @@ import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    Optional<Pedido> findByIdAndDeletedFalse(Long id);
+    @Query("SELECT p FROM Pedido p LEFT JOIN FETCH p.lineasVenta WHERE p.id = :id AND p.deleted = false")
+    Optional<Pedido> findByIdAndDeletedFalse(@Param("id") Long id);
+
+    @Query("SELECT DISTINCT p FROM Pedido p LEFT JOIN FETCH p.lineasVenta WHERE p.deleted = false")
     List<Pedido> findAllByDeletedFalse();
 }
