c0773aefeebc9837567d78d888c5e2ab90af3b3f
Raul Fernandez <raulfernandez6106@gmail.com>
2025-05-20 17:43:09 +0200

    Configurando opcion compra

--- src/main/java/examen/dev/tfgalmacen/rest/clientes/controller/ClienteController.java
+++ src/main/java/examen/dev/tfgalmacen/rest/clientes/controller/ClienteController.java
@@ -3,6 +3,9 @@ package examen.dev.tfgalmacen.rest.clientes.controller;
 import examen.dev.tfgalmacen.rest.clientes.dto.ClienteRequest;
 import examen.dev.tfgalmacen.rest.clientes.dto.ClienteResponse;
 import examen.dev.tfgalmacen.rest.clientes.service.ClienteService;
+import examen.dev.tfgalmacen.rest.pedido.dto.CompraRequest;
+import examen.dev.tfgalmacen.rest.pedido.dto.PedidoResponse;
+import examen.dev.tfgalmacen.rest.pedido.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,44 +15,58 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
-@PreAuthorize("hasAnyRole('ADMIN', 'TRABAJADOR')")
 public class ClienteController {
 
     private final ClienteService clienteService;
+    private final PedidoService pedidoService;
 
     @Autowired
-    public ClienteController(ClienteService clienteService) {
+    public ClienteController(ClienteService clienteService, PedidoService pedidoService) {
         this.clienteService = clienteService;
+        this.pedidoService = pedidoService;
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRABAJADOR')")
     public ResponseEntity<List<ClienteResponse>> getAllClientes() {
         List<ClienteResponse> clientes = clienteService.getAllClientes();
         return ResponseEntity.ok(clientes);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRABAJADOR')")
     public ResponseEntity<ClienteResponse> getClienteById(@PathVariable Long id) {
         ClienteResponse cliente = clienteService.getById(id);
         return ResponseEntity.ok(cliente);
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRABAJADOR')")
     public ResponseEntity<ClienteResponse> createCliente(@RequestBody ClienteRequest clienteRequest) {
         ClienteResponse nuevoCliente = clienteService.createCliente(clienteRequest);
         return ResponseEntity.status(201).body(nuevoCliente);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRABAJADOR')")
     public ResponseEntity<ClienteResponse> updateCliente(@PathVariable Long id, @RequestBody ClienteRequest clienteRequest) {
         ClienteResponse clienteActualizado = clienteService.updateCliente(id, clienteRequest);
         return ResponseEntity.ok(clienteActualizado);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRABAJADOR')")
     public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
         clienteService.deleteCliente(id);
         return ResponseEntity.noContent().build();
     }
-}
 
+    @PostMapping("/{id}/comprar")
+    @PreAuthorize("hasAuthority('CLIENTE')")
+
+    public ResponseEntity<PedidoResponse> comprarProducto(@PathVariable("id") Long id, @RequestBody CompraRequest request) {
+        request.setClienteId(id);
+        PedidoResponse response = pedidoService.crearCompraDesdeNombreProducto(request);
+        return ResponseEntity.ok(response);
+    }
+}
