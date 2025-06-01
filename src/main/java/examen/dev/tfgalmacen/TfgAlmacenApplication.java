3677d23678bb94ab5d8193ca8969bdeb009fd885
Raul Fernandez <raulfernandez6106@gmail.com>
2025-06-01 17:21:00 +0200

    Desplieguehecho

--- src/main/java/examen/dev/tfgalmacen/TfgAlmacenApplication.java
+++ src/main/java/examen/dev/tfgalmacen/TfgAlmacenApplication.java
@@ -2,10 +2,14 @@ package examen.dev.tfgalmacen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TfgAlmacenApplication {
+public class TfgAlmacenApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(TfgAlmacenApplication.class, args);
+        System.out.println("Accede a Swagger en: http://localhost:8080/swagger-ui/index.html#/");
+        System.out.println("Accede a Jacoco en: http://localhost:8080/jacoco/index.html\n");
     }
 }
