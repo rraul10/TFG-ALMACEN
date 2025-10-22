194286175582f7687b2bf9d538acae868334e7d3
Raúl Fernández <146001066+rraul10@users.noreply.github.com>
2025-10-22 18:23:48 +0200

    Cambios

--- almacen-frontend/src/main.ts
+++
@@ -1,7 +0,0 @@
-// src/main.ts
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
