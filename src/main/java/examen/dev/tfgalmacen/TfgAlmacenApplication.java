2d8e1d6c53c8a25a4d65d3b50fceb07a4147ee84
Raul Fernandez <raulfernandez6106@gmail.com>
2025-11-02 17:17:38 +0100

    arreglos Arreglos y limpieza de configuración

--- src/main/java/examen/dev/tfgalmacen/TfgAlmacenApplication.java
+++ src/main/java/examen/dev/tfgalmacen/TfgAlmacenApplication.java
@@ -4,12 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@SpringBootApplication(scanBasePackages = "examen.dev.tfgalmacen")
 public class TfgAlmacenApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // Carga manual de .env
+        loadEnv();
+
         SpringApplication.run(TfgAlmacenApplication.class, args);
         System.out.println("Accede a Swagger en: http://localhost:8080/swagger-ui/index.html#/");
         System.out.println("Accede a Jacoco en: http://localhost:8080/jacoco/index.html\n");
     }
+
+    private static void loadEnv() throws IOException {
+        Properties props = new Properties();
+        FileInputStream fis = new FileInputStream(".env");
+        props.load(fis);
+        fis.close();
+
+        // Variables disponibles para Spring
+        System.setProperty("jwt.secret", props.getProperty("JWT_SECRET"));
+        System.setProperty("jwt.expiration", "3600");
+        System.setProperty("spring.datasource.username", props.getProperty("DB_USER"));
+        System.setProperty("spring.datasource.password", props.getProperty("DB_PASSWORD"));
+        System.setProperty("spring.mail.username", props.getProperty("EMAIL_USER"));
+        System.setProperty("spring.mail.password", props.getProperty("EMAIL_PASSWORD"));
+        System.setProperty("stripe.secret.key", props.getProperty("STRIPE_SECRET_KEY"));
+    }
 }
+
